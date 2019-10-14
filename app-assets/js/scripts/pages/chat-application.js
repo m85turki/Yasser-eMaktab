b2e5974909bcdd76dad38f321f08aec682e53e09
m85turki <mohammadt@sssit.net>
2019-10-14 11:21:55 +0300

    no message

---
+++ app-assets/js/scripts/pages/chat-application.js
@@ -0,0 +1,6 @@
+//  Notifications & messages scrollable
+if($('.sidebar-fixed, .scrollbar-p').length > 0){
+    $('.sidebar-fixed, .scrollbar-p').perfectScrollbar({
+        theme:"dark"
+    });
+}
